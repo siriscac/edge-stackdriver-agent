575664983493f2db92a02190e16cb9ff0a1b6607
Muthuramakrishnan <mviswanathan@google.com>
2017-05-16 10:57:19 +0800

    Update agent

--- Dockerfile
+++ Dockerfile
@@ -5,13 +5,10 @@ RUN apt-get -qq -y install curl
 
 RUN curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 RUN apt-get install -y nodejs
-
 RUN apt-get install -y rsyslog
-CMD rsyslogd -n
+RUN npm i -g pm2
 
 COPY rsyslog.conf /etc/rsyslog.conf
-RUN service rsyslog restart
-RUN npm i -g pm2
 COPY /agent /home/agent
 
 WORKDIR /home/agent
@@ -20,4 +17,4 @@ RUN npm install
 EXPOSE 514/udp
 EXPOSE 601/tcp
 
-CMD ["pm2", "start", "agent.js", "--no-daemon"]
+CMD service rsyslog restart && pm2 start agent.js --no-daemon
--- agent/agent.js
+++ agent/agent.js
@@ -1,11 +1,13 @@
 var Tail = require('tail').Tail;
 tailf = new Tail("/var/log/syslog");
 
+var private_value = process.env.GSRV_PRIVATE_KEY.replace(/\\n/g, '\n');
+
 var servicekey = {
   "type": process.env.GSRV_TYPE,
   "project_id": process.env.GSRV_PROJECT_ID,
   "private_key_id": process.env.GSRV_PRIVATE_KEY_ID,
-  "private_key": process.env.GSRV_PRIVATE_KEY,
+  "private_key": private_value,
   "client_email": process.env.GSRV_CLIENT_EMAIL,
   "client_id": process.env.GSRV_CLEINT_ID,
   "auth_uri": process.env.GSRV_AUTH_URI,
@@ -14,7 +16,14 @@ var servicekey = {
   "client_x509_cert_url": process.env.GSRV_CLIENT_CERT_URL
 }
 
-console.log(servicekey);
+var key = JSON.stringify(servicekey);
+
+var fs = require('fs');
+fs.writeFile('servicekey.json', key, function (err) {
+  if (err) return console.log(err);
+});
+
+console.log(key);
 
 var loggingClient = require('@google-cloud/logging')({
   credentials: servicekey
